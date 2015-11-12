700a15d482ed71ecfffb155c00ffccfa24782080
Schmidt-A <caffeinated.tweek@gmail.com>
2015-11-12 11:26:50 -0800

    Function to get matchinfo once a match is complete. TODO: Error validate.

--- league_tourneys/tournaments/lib/tournament_api.py
+++ league_tourneys/tournaments/lib/tournament_api.py
@@ -5,12 +5,16 @@ key = keys['riot_games_api_key']
 
 host                = 'http://52.33.251.214'
 url_base            = 'https://global.api.pvp.net{0}'
+na_url_base         = 'https://na.api.pvp.net{0}'
 provider_url_base   = url_base.format('/tournament/public/v1/provider')
 tournament_url_base = url_base.format('/tournament/public/v1/tournament')
 code_url_base       = url_base.format('/tournament/public/v1/code')
-t_match_url_base    = url_base.format('/api/lol/NA/v2.2/match/for-tournament/{0}')
+t_match_url_base    = na_url_base.format('/api/lol/na/v2.2/match/by-tournament/{0}/ids?')
+match_info_url_base = na_url_base.format('/api/lol/na/v2.2/match/for-tournament/{0}')
+
 
 header = {'X-Riot-Token': key, 'Content-Type': 'application/json'}
+header_no_token = {'Content-Type': 'application/json'}
 
 def register_new_provider(provider_account):
     json_data = {'region': 'NA', 'url': host}
@@ -24,8 +28,8 @@ def create_tournament(name, provider_id):
 
     return r.text
 
-def create_match(tournament_id, num_matches):
-    params = {'tournamentId': tournament_id, 'count': num_matches}
+def create_match(tournament_code, num_matches):
+    params = {'tournamentId': tournament_code, 'count': num_matches}
     json_data = {
             'teamSize': 5,
             'participants': [0],
@@ -38,18 +42,17 @@ def create_match(tournament_id, num_matches):
             json=json_data)
     return r.json()[0]
 
-def get_initial_info(match_id):
-    url = match_url_base.format(match_id)
-    #r = requests.get(url, 
+def get_match_id(tournament_code):
+    url = t_match_url_base.format(tournament_code)
+    r = requests.get(url, headers=header_no_token, params={'api_key': key})
+    match_id = r.json()[0]
+    return match_id
 
+def get_match_info(tournament_code):
+    match_id = get_match_id(tournament_code)
 
-
-# Sample going from beginning to end
-#new_p_id = register_new_provider('Tweeks')
-#t_id = create_tournament('test-tourney', new_p_id)
-
-# just for now
-t_id = 1795
-m_id = 'NA0416f-ab701d93-089d-4c34-bf5a-0cd2193b0cef'
-#m_id = create_match(t_id, 1)
-print m_id
+    params = {'api_key': key, 'tournamentCode': tournament_code}
+    url = match_info_url_base.format(match_id)
+    r = requests.get(url, params=params)
+    match_info = r.json()
+    return match_info
