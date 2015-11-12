9db763575eab955a3117a138e847dd00c9503cf4
Russ M <russmilne@gmail.com>
2015-11-12 11:22:51 -0600

    - fixed some syntax things and spelling

--- league_tourneys/tournaments/views.py
+++ league_tourneys/tournaments/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 
-from .models import Tournament, Match
+from models import Tournament, Match
 
 # Create your views here.
 def index(request):
     return HttpResponse("hello rghack2015")
 
-class TournamemtDetailView(generic.DetailView):
+class TournamentDetailView(generic.DetailView):
     model = Tournament
-    template_name = 'temaplates/tournament/detail.html'
+    template_name = 'templates/tournament/detail.html'
 
 class MatchDetailView(generic.DetailView):
     model = Match
-    template_name = 'temaplates/match/detail.html'
+    template_name = 'templates/match/detail.html'
