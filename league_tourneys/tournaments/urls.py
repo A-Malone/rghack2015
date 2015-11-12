6fadec1ef67cc2b7ce1c1c537d93c0f80f722f9d
Russ M <russmilne@gmail.com>
2015-11-12 13:45:56 -0600

    - added notification model, url to receive POSTs from rito

--- league_tourneys/tournaments/urls.py
+++ league_tourneys/tournaments/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.TournamentDetailView.as_view(), name='tournament')
+    url(r'^(?P<pk>[0-9]+)/$', views.TournamentDetailView.as_view(),
+        name='tournament'),
+    url(r'^/notification', views.notification, name='notification'),
 ]
