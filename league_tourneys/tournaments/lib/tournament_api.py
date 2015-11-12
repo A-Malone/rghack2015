29aa4b9751691eac4ee598ea98efde694df1f9b2
Schmidt-A <caffeinated.tweek@gmail.com>
2015-11-12 10:03:49 -0800

    Temporary commit so everything is up to date.

--- league_tourneys/tournaments/lib/tournament_api.py
+++ league_tourneys/tournaments/lib/tournament_api.py
@@ -3,11 +3,14 @@ from key_manager import keys
 
 key = keys['riot_games_api_key']
 
-host = 'http://52.33.251.214'
-t_url_base = 'https://global.api.pvp.net{0}'
-provider_url_base = t_url_base.format('/tournament/public/v1/provider')
-tournament_url_base = t_url_base.format('/tournament/public/v1/tournament')
-code_url_base = t_url_base.format('/tournament/public/v1/code')
+host                = 'http://52.33.251.214'
+url_base            = 'https://global.api.pvp.net{0}'
+provider_url_base   = url_base.format('/tournament/public/v1/provider')
+tournament_url_base = url_base.format('/tournament/public/v1/tournament')
+code_url_base       = url_base.format('/tournament/public/v1/code')
+t_match_url_base    = url_base.format('/api/lol/NA/v2.2/match/for-tournament/{0}')
+
+header = {'X-Riot-Token': key, 'Content-Type': 'application/json'}
 
 def register_new_provider(provider_account):
     json_data = {'region': 'NA', 'url': host}
@@ -26,16 +29,27 @@ def create_match(tournament_id, num_matches):
     json_data = {
             'teamSize': 5,
             'participants': [0],
-            'spectatorType': 'LOBBYONLY',
+            'spectatorType': 'ALL',
             'pickType': 'TOURNAMENT_DRAFT',
             'mapType': 'SUMMONERS_RIFT',
             'metadata': ''
             }
     r = requests.post(code_url_base, headers=header, params=params,
             json=json_data)
-    return r.text
+    return r.json()[0]
+
+def get_initial_info(match_id):
+    url = match_url_base.format(match_id)
+    #r = requests.get(url, 
+
+
 
 # Sample going from beginning to end
 #new_p_id = register_new_provider('Tweeks')
 #t_id = create_tournament('test-tourney', new_p_id)
-#m_idcreate_match(t_id, 1)
+
+# just for now
+t_id = 1795
+m_id = 'NA0416f-ab701d93-089d-4c34-bf5a-0cd2193b0cef'
+#m_id = create_match(t_id, 1)
+print m_id
