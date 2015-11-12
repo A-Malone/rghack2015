5dada63069df25324e7059900ec8ce511055c573
Russ M <russmilne@gmail.com>
2015-11-12 11:00:41 -0600

    - fix import syntax fix with tournaments.urls

--- league_tourneys/tournaments/urls.py
+++ league_tourneys/tournaments/urls.py
@@ -1,8 +1,8 @@
-import django.conf.urls import url
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^/tournaments/(?P<pk>[0-9]+)/$', views.TournamemtDetailView.as_view(), name='tournament'),
+    url(r'^/tournaments/(?P<pk>[0-9]+)/$', views.TournamemtDetailView.as_view(), name='tournament')
 ]
