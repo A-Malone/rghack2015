f34466e4fad449b6ddac280015cb4945e795572b
A. Malone <a_s_malone@hotmail.com>
2015-11-12 20:55:42 -0800

    Started pipecleaning internals

--- league_tourneys/tournaments/forms.py
+++ league_tourneys/tournaments/forms.py
@@ -14,3 +14,4 @@ class TournamentForm(forms.Form):
 class TeamForm(forms.Form):
     name                = forms.CharField(label='Team Name', max_length=100)
     members             = forms.CharField(label='Team Members', max_length=200)
+
--- league_tourneys/tournaments/views.py
+++ league_tourneys/tournaments/views.py
@@ -1,7 +1,7 @@
 import pprint
 import json
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
@@ -10,6 +10,8 @@ from django.core.urlresolvers import reverse
 from models import Tournament, Match, Notification, Team, Summoner
 
 from .forms import TournamentForm, TeamForm
+
+from lib import challonge_api
 
 
 # Create your views here.
@@ -24,15 +26,53 @@ def notification(request):
         if 'shortCode' in data:
             Notification.objects.create(tournament_code=data['shortCode'],
                                         json_text=request.body)
+            
+            short_code = data['short_code']
+            match = Match.objects.get(tournament_api_match_id=short_code)
+
+            teams = match.teams.all()
+
+            winning_player = data["winningTeam"][0]["summonerId"]
+            if(winning_player in map(lambda x: x.summoner_id, teams[0].summoners.all())):
+                winning_team = teams[0]
+                losing_team = teams[1]
+            else:                
+                winning_team = teams[1]
+                losing_team = teams[0]
+
+            challonge_match_id = match.challonge_match_id
+            swap = winning_team.pk == match.first_team_id
+            match_results = {"scores_csv":"1-0" if swap else "0-1", "winner_id": winning_team.challonge_team_id}
+            challonge_api.update_match(match.tournament_id, match.challonge_match_id, {scores})
+            print("Updated match {}".format(match_results))
+
         pprint.pprint(data)
         return HttpResponse('Post json: ' + pprint.pformat(data))
     return HttpResponse('only POST requests pls rito')
-
 
 # TOURNAMENT CONTROLLER
 class TournamentDetailView(generic.DetailView):
     model = Tournament
     template_name = "tournament/detail.html"
+
+def start_tournament(request, tournament_id):
+    tournament = Tournament.objects.get(pk=int(tournament_id))
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        challonge_api.start_tournament(tournament.challonge_tournament_id)
+
+        p = challonge_api.get_match_list(tournament.challonge_tournament_id, state="open")
+        print(p)
+
+        for match_json in p:
+            challonge_match_id = match_json["match"]["id"]
+            player1_id = match_json["match"]["player1_id"]
+            player2_id = match_json["match"]["player2_id"]
+
+            match = Match.create(tournament_id, challonge_match_id, player1_id, player2_id)
+            tournament.match_set.add(match)
+    
+    return redirect('tournament', pk=tournament_id)
 
 def create_tournament(request):
     # if this is a POST request we need to process the form data
@@ -50,10 +90,15 @@ def create_tournament(request):
         form = TournamentForm()
     return render(request, "tournament/create.html", {"form": form, "action":reverse("create_tournament")})
 
+
 # MATCH CONTROLLER
-class MatchDetailView(generic.DetailView):
-    model = Match
-    template_name = "match/detail.html"
+def match_detail_view(request, tournament_id, match_id):
+    tournament = Tournament.objects.get(pk=int(tournament_id))
+    match = tournament.match_set.filter(pk=int(match_id))
+
+    context = {'match':match, 'teams':match.teams.all()}
+    return render(request, "match/detail.html", context)
+ 
 
 # TEAMS CONTROLLER
 def create_team(request, tournament_id):
